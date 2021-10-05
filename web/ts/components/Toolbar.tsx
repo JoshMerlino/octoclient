f380383c0238510ec225f9ff5cbc06836d8f745c
Josh Merlino <joshua.merlino@gmail.com>
2021-10-04 21:43:08 -0400

    add theme switcher, its night and im not tryin to go blind

--- web/ts/components/Toolbar.tsx
+++ web/ts/components/Toolbar.tsx
@@ -1,11 +1,15 @@
-import { Toolbar, ToolbarTitle } from "photoncss/lib/react";
+import { Toolbar, ToolbarActions, ToolbarTitle } from "photoncss/lib/react";
 import React from "react";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Component(): JSX.Element {
 	return (
 		<>
 			<Toolbar>
 				<ToolbarTitle>{ APP_MANIFEST.name }</ToolbarTitle>
+				<ToolbarActions>
+					<ThemeSwitcher/>
+				</ToolbarActions>
 			</Toolbar>
 		</>
 	);
