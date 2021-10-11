88bb8c3a605c2c3ec6127b4d4fbf5442e53d0e04
Josh Merlino <joshua.merlino@gmail.com>
2021-10-11 09:38:23 -0400

    update control panel

--- web/ts/components/panel/Connection.tsx
+++ web/ts/components/panel/Connection.tsx
@@ -1,14 +1,17 @@
-import { Button, Card, CardActions, CardTitle, InputField, Spinner } from "photoncss/lib/react";
+import { Button, Card, CardActions, CardTitle, InputField, Snackbar, Spinner } from "photoncss/lib/react";
 import React, { useState } from "react";
+import Photon from "photoncss";
 import useAPI from "runtime/util/hooks/useAPI";
 import api from "runtime/util/api";
 
 export default function ConnectionPanel(): JSX.Element {
 
+	// Initialize states.
 	const [ connection, refreshConnection ] = useAPI<OctoConnection>("/api/connection");
 	const [ isChanged, setChanged ] = useState(false);
 	const [ isConnected, setConnected ] = useState<null | boolean>(null);
 
+	// Show loading spinner if it hasn't resolved yet.
 	if (!connection) return (
 		<Card variant="outlined">
 			<CardTitle subtitle="Loading...">Connection</CardTitle>
@@ -18,16 +21,29 @@ export default function ConnectionPanel(): JSX.Element {
 		</Card>
 	);
 
+	// Initialize connection.
 	if (connection?.current?.state === "Detecting serial connection") setTimeout(refreshConnection, 1000);
 	if (isConnected === null) setConnected(connection?.current?.state === "Operational");
 
-	const buttonEnabled = (isConnected || isChanged) && !(connection?.current?.state === "Detecting serial connection");
+	// Should button be enabled based on the state.
+	const buttonEnabled = (isConnected || isChanged) && !(connection?.current?.state === "Detecting serial connection") && !(connection?.current?.state === "Printing");
 
+	// Function to manage the connection.
 	async function connect() {
+
+		// If the printer is printing, dont fuck with the connection.
+		if (connection && connection?.current?.state === "Printing") return Photon.Snackbar(<Snackbar>
+			<p>Printer is currently printing. You must end the current print before the connection is closed.</p>
+		</Snackbar>, { duration: 10000 });
+
+		// If its not connected, then connect
 		if (!isConnected) {
+
+			// Get information from dropdowns
 			const port = $("#serial-port").val();
 			const baudratePreference = $("#baud-rate").val();
 
+			// POST to the server
 			await api("/api/connection", {
 				method: "POST",
 				body: JSON.stringify({
@@ -39,10 +55,17 @@ export default function ConnectionPanel(): JSX.Element {
 				})
 			});
 
+			// Update state
 			setChanged(false);
 			setConnected(true);
-		} else {
 
+		// eslint-disable-next-line brace-style
+		}
+
+		// If its already connected, then disconnect it
+		else {
+
+			// Disconnect the printer
 			await api("/api/connection", {
 				method: "POST",
 				body: JSON.stringify({
@@ -50,21 +73,33 @@ export default function ConnectionPanel(): JSX.Element {
 				})
 			});
 
+			// Update state
 			setConnected(false);
 			setChanged(true);
+
 		}
+
+		// Refresh the connection status with the latest information from the server.
 		refreshConnection();
+
 	}
 
+	// Return UI
 	return (
 		<Card variant="outlined">
 			<CardTitle subtitle={connection.current.state}>Connection</CardTitle>
+
+			{ connection.current.state === "Printing" && <>
+				<p>Connection information can not be changed while the printer is printing.</p>
+				<hr style={{ marginTop: 12 }} />
+			</> }
 
 			<div className="card-input-wrapper">
 				<InputField
 					variant="outlined"
 					color="primary"
 					id="serial-port"
+					disabled={connection.current.state === "Printing"}
 					onChange={ () => setChanged(true) }
 					defaultValue={connection.current.port}
 					dropdown={connection.options.ports}>Serial Port</InputField>
@@ -74,6 +109,7 @@ export default function ConnectionPanel(): JSX.Element {
 					variant="outlined"
 					color="primary"
 					id="baud-rate"
+					disabled={connection.current.state === "Printing"}
 					onChange={ () => setChanged(true) }
 					defaultValue={connection.current.baudrate}
 					dropdown={connection.options.baudrates.map(a => a.toString()).reverse()}>Baud Rate</InputField>
