9041954f3060e07551576d2ef0c698b51c80e3e6
Josh Merlino <joshua.merlino@gmail.com>
2021-10-17 14:24:41 -0400

    fix key not used error

--- web/ts/components/FileExplorer/FileRoot.tsx
+++ web/ts/components/FileExplorer/FileRoot.tsx
@@ -7,8 +7,8 @@ export type Props = { files: Octo.File[], free: number };
 export default function FileRoot({ files }: Props): JSX.Element {
 	return (
 		<List style={{ margin: " -20px 0", border: "none", maxHeight: 400, overflow: "auto"}}>
-			{ files.map(file => {
-				return <FileEntry {...file} />;
+			{ files.map((file, key) => {
+				return <FileEntry key={key} {...file} />;
 			})}
 		</List>
 	);
